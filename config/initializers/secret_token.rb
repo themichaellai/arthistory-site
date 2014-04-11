cb6f67459bbb86faafc1cfc0f23086247973e8d0
Michael Lai <themichaellai@gmail.com>
2014-04-10 21:57:04 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArthistSite::Application.config.secret_token = 'de80e155de4da1996e1247ffd7ed2cf82fb30f3ab74ce22d494b1a63e43fa888a45f448877d6ad62dd1ffd691fe6b149fd4cab8812985f40684f2ae2209f27c9'
